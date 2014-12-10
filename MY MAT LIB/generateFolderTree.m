a3863d0c1e2fee3c59eebd2881b003992974de8b
OleNet <olenet@126.com>
2014-12-10 16:37:03 +0800

    add some py files.

--- MY MAT LIB/generateFolderTree.m
+++ MY MAT LIB/generateFolderTree.m
@@ -1,7 +1,12 @@
-function [folders] = generateFolderTree( root)
+function [folders] = generateFolderTree(root, depth, exclude_folder)
 %%
-if nargin ==1
+if nargin < 3
     exclude_folder = [];
+end
+
+if (depth-1 == 0)
+    folders = [];
+    return 
 end
 
 path = root;
@@ -9,13 +14,17 @@ files = dir(root);
 
 folders = {};
 for i = 3 : length(files)
-    if ~isdir([root '\' files(i).name])% || sum(exclude_folder == files(i).name)~=0
+    if ~isdir([root '\' files(i).name]) || ~isempty(strfind(exclude_folder, files(i).name))
        continue;
     else
-         sub_folder = [root '\' files(i).name];
+        sub_folder = [root '\' files(i).name];
         folders{end+1} = sub_folder;
     end
-    new_folders = generateFolderTree(sub_folder);%, exclude_folder);
+    
+    new_folders = generateFolderTree(sub_folder, depth-1, exclude_folder);
+    if isempty(new_folders)
+        continue;
+    end
     folders = [folders, new_folders];    
 end
 
